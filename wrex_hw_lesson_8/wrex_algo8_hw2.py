e20016ba01c37f0de610cf9ebbc855be0366799c
Wrexan <fwrexan@gmail.com>
2021-07-22 23:50:56 +0300

    upd 8.2

--- wrex_hw_lesson_8/wrex_algo8_hw2.py
+++ wrex_hw_lesson_8/wrex_algo8_hw2.py
@@ -9,6 +9,7 @@
 #                                     Данные
 import random as rand
 
+
 words_1 = ['Аист', 'Берёза', 'Вася', 'Годзилла', 'Дочка',
            'Ёжик', 'Жена', 'Зёбра', 'Игнат', 'Кеша',
            'Ласка', 'Мама', 'Носорог', 'Петя', 'Тётя']
@@ -25,6 +26,33 @@ def rnd(a: list, b: list, c: list):
 
 
 # -----------------------------------------------------------------------------
+#                                    Древо
+class NodeTree(object):
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return self.left, self.right
+
+    def nodes(self):
+        return self.left, self.right
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+
+def huffman_code_tree(node, left=True, binary_string=''):
+    if type(node) is str:
+        return {node: binary_string}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binary_string + '0'))
+    d.update(huffman_code_tree(r, False, binary_string + '1'))
+    return d
+
+
+# -----------------------------------------------------------------------------
 #                                    Генератор весов
 def get_weights(st: str):
     wt = {}
@@ -33,13 +61,20 @@ def get_weights(st: str):
             wt[s] += 1
         else:
             wt[s] = 1
-    wt = sorted(wt.items(), key=lambda x: x[1])
+    wt = sorted(wt.items(), key=lambda x: x[1], reverse=True)
     return wt
 
 
-# -----------------------------------------------------------------------------
-#  Распускаем стринг на подстринги, проверяем часть в общем через хеш, исключаем повтор
-# def
+def to_nodes(nodes: list):
+    while len(nodes) > 1:
+        (key1, c1) = nodes[-1]
+        (key2, c2) = nodes[-2]
+        nodes = nodes[:-2]
+        node = NodeTree(key1, key2)
+        nodes.append((node, (c1 + c2)))
+
+        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+    return nodes
 
 
 # -----------------------------------------------------------------------------
@@ -48,6 +83,10 @@ def main(st: str):
     print(f'{st=}')
     w_dict = get_weights(st)
     print(f'{w_dict=} ')
+    nodes = to_nodes(w_dict)
+    huffman_code = huffman_code_tree(nodes[0][0])
+    for (char, frequency) in w_dict:
+        print(' %-4r |%12s' % (char, huffman_code[char]))
 
 
 # -----------------------------------------------------------------------------
